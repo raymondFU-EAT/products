c43b72623358265ff8e18d4ffd6cb5e00e4fb876
Raymond BBF <ketchup942@gmail.com>
2021-03-04 21:19:02 +0800

    'write_p_easy'

--- products.py
+++ products.py
@@ -6,5 +6,10 @@ while True:
 	price = input('請輸入商品價格')
 	products.append([name, price])
 print(products)
+
 for p in products:
 	print('商品', p[0], '價格', p[1])
+
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] +  ',' + p[1] + '\n')
