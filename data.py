c2c338eb1118c2eb725d90fcfa2d18ae636dc6b8
Raymond BBF <ketchup942@gmail.com>
2021-03-05 17:22:37 +0800

    'refactor'

--- data.py
+++ data.py
@@ -1,6 +1,49 @@
-data = [1, 3, 5, 7, 9]
+import os # operating system
 
-with open('test.csv', 'w') as f:
-	for p in data:
-		p = str(p)
-		f.write(str(p[0])+'\n')
+#讀取檔案
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding = 'utf-8')as f:
+        for line in f:
+            if line == '商品, 價格':
+                continue # 繼續,跳到下一行
+            name, price = line.strip().split(',')
+            products.append([name, price])    
+    return products
+
+# 讓使用者輸入
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格')
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
+
+# 印出所有購買紀錄
+def print_file(products):
+    for p in products:
+        print(p[0], '的價格是', p[1])
+
+# 寫入檔案
+def write_file(filename, products):
+    with open(filename, 'w', encoding='utf-8')as f:
+        f.write('商品, 價格\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename): # 檢查檔案在不在
+        print('讀取成功')
+        products = read_file(filename)
+    else:
+        print('讀取失敗')
+
+    products = user_input(products)
+    print_file(products)
+    write_file('products.csv', products)
+main()
